764c3fe9cea00bdbb26694ac589d3d2f11eeb9cc
matbarPL <mateusz.baryla.1996@gmail.com>
2021-05-08 01:36:25 +0200

    Create sel.py

---
+++ selenium/sel.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import pandas as pd
+
+chrome_path = '/Users/mateusz/Documents/Learning/WSSMS/class_07/chromedriver'
+url = 'https://conotoxia.com/cryptocurrencies/cryptocurrency-rates'
+
+LIMIT_PAGES = 4
+url = 'https://conotoxia.com/cryptocurrencies/cryptocurrency-rates'
+urls = [url+'/p/'+str(i) for i in range(1, LIMIT_PAGES+1)]
+names = ["currency", "average_rate", "market_value", "volume", "exchange"]
+df = pd.DataFrame(dict(zip(names, [[]]*4 )))
+
+for url in urls:
+    chrome_options = Options()  
+    chrome_options.add_argument("--headless")  
+    driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_path)
+    driver.get(url)
+
+    for tr in driver.find_elements_by_tag_name("tr")[1:]:
+        values = []
+        for td in tr.find_elements_by_tag_name("td"):
+            attr = td.get_attribute("innerText")
+            val = ''.join([x for x in attr if ord(x) < 127]).replace("\n","")
+            values.append(val)
+
+        row = dict(zip(names, values))
+        df = df.append(row, ignore_index=True)
+df.to_csv("project_scrapping//selenium//crypto.csv", index=False)
+driver.quit()
