6c064e3d37ce9505152634e96e0241fe04ec72b6
matbarPL <mateusz.baryla.1996@gmail.com>
2021-05-08 22:06:48 +0200

    Update soup.py

--- soup/soup.py
+++ soup/soup.py
@@ -1,44 +1,28 @@
-# -*- coding: utf-8 -*-
-import scrapy
+from bs4 import BeautifulSoup as BS
+from numpy import column_stack
+from urllib import request as re
+import pandas as pd
+from urllib.request import Request, urlopen
 
-class Link(scrapy.Item):
-    currency = scrapy.Field()
-    average_rate = scrapy.Field()
-    market_value = scrapy.Field()
-    volume = scrapy.Field()
-    exchange = scrapy.Field()
+LIMIT_PAGES = 4
+url = 'https://conotoxia.com/cryptocurrencies/cryptocurrency-rates'
+urls = [url+'/p/'+str(i) for i in range(1, LIMIT_PAGES+1)]
+print(urls)
 
-class LinkListsSpider(scrapy.Spider):
-    name = 'link_lists'
-    allowed_domains = ['https://conotoxia.com']
-    start_urls = ['https://conotoxia.com/cryptocurrencies/cryptocurrency-rates']
-    LIMIT_PAGE = 2
-    COUNTER = 1
+names = ["currency", "average_rate", "market_value", "volume", "exchange"]
+df = pd.DataFrame(dict(zip(names, [[]]*4 )))
 
-    def parse(self, response):
-        print(response)
-        trs = response.xpath('/html/body/main/div/section[1]/div/div/table//tr')
-        link = Link()
-        for row in trs[1:]:
-            currency = ''.join(row.xpath('td[1]//text()').extract()).replace("\n","")
-            link['currency'] = ''.join([x for x in currency if ord(x) < 127]).replace("\n","")
-            average_rate = ''.join(row.xpath('td[2]//text()').extract()).replace("\n","")
-            link['average_rate'] = ''.join([x for x in average_rate if ord(x) < 127]).replace("\n","")
-            market_value = ''.join(row.xpath('td[3]//text()').extract()).replace("\n","")
-            link['market_value'] = ''.join([x for x in market_value if ord(x) < 127]).replace("\n","")
-            volume = ''.join(row.xpath('td[4]//text()').extract()).replace("\n","")
-            link['volume'] = ''.join([x for x in volume if ord(x) < 127]).replace("\n","")
-            exchange = ''.join(row.xpath('td[5]//text()').extract()).replace("\n","")
-            link['exchange'] = ''.join([x for x in exchange if ord(x) < 127]).replace("\n","")
-            yield link
+for url in urls:
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+    soup = BS(webpage, 'html.parser')
 
-        xpath = "/html/body/main/div/section[1]/div/nav/ul/li[5]/a/@href"
-        next_page=response.xpath(xpath).extract()
-        print(self.COUNTER < self.LIMIT_PAGE)
-
-        if self.COUNTER < self.LIMIT_PAGE:
-            self.COUNTER += 1
-            next_page_link = 'https://conotoxia.com/cryptocurrencies/cryptocurrency-rates/p/' + str(self.COUNTER)
-            print(next_page_link)
-            yield scrapy.Request(url=next_page_link, callback= self.parse)
-        
+    for tr in soup.find_all('tr')[1:]:
+        tds = tr.find_all('td')
+        values = []
+        for i in range(len(tds)):
+            values.append(''.join([x for x in tds[i].text if ord(x) < 127]).replace("\n",""))
+        row = dict(zip(names, values))
+        print(row)
+        df = df.append(row, ignore_index=True)
+df.to_csv("project_scrapping//bs//crypto.csv", index=False)
