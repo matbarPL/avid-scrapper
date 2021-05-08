3662a336beb2db422cc19d0f6e28f824c26e75a1
matbarPL <mateusz.baryla.1996@gmail.com>
2021-05-08 22:19:15 +0200

    Update soup.py

--- soup/soup.py
+++ soup/soup.py
@@ -7,7 +7,6 @@ from urllib.request import Request, urlopen
 LIMIT_PAGES = 4
 url = 'https://conotoxia.com/cryptocurrencies/cryptocurrency-rates'
 urls = [url+'/p/'+str(i) for i in range(1, LIMIT_PAGES+1)]
-print(urls)
 
 names = ["currency", "average_rate", "market_value", "volume", "exchange"]
 df = pd.DataFrame(dict(zip(names, [[]]*4 )))
@@ -23,6 +22,6 @@ for url in urls:
         for i in range(len(tds)):
             values.append(''.join([x for x in tds[i].text if ord(x) < 127]).replace("\n",""))
         row = dict(zip(names, values))
-        print(row)
         df = df.append(row, ignore_index=True)
+        
 df.to_csv("crypto.csv", index=False)
